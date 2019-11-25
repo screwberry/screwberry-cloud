db06479ccee266be898823b95828723b5593111a
screwberry <mikko.m.kallio@gmail.com>
2019-11-25 23:40:33 +0200

    Add custom script extension

--- main.tf
+++ main.tf
@@ -21,6 +21,8 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.resource_group_location}"
 }
 
+# IoT Hub resources
+
 resource "azurerm_storage_account" "sta" {
   name                     = "${var.resource_prefix}storage"
   resource_group_name      = "${azurerm_resource_group.rg.name}"
@@ -63,4 +65,107 @@ resource "azurerm_iothub_consumer_group" "cg" {
   iothub_name            = "${azurerm_iothub.iothub.name}"
   eventhub_endpoint_name = "events"
   resource_group_name    = "${azurerm_resource_group.rg.name}"
+}
+
+# Virtual machine resources
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.resource_prefix}-subnet"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                    = "${var.resource_prefix}-pip"
+  location                = "${azurerm_resource_group.rg.location}"
+  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    project     = "screwberry"
+    environment = "${var.environment}"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.resource_prefix}-nic"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  ip_configuration {
+    name                          = "${var.resource_prefix}-ipconfig"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.pip.id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "${var.resource_prefix}-vm"
+  location              = "${azurerm_resource_group.rg.location}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  vm_size               = "Standard_B1S"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.resource_prefix}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    project     = "screwberry"
+    environment = "${var.environment}"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "ext" {
+  name                 = "docker-grafana"
+  location             = "${azurerm_resource_group.rg.location}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_machine_name = "${azurerm_virtual_machine.vm.name}"
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+  {
+  "fileUris": ["https://raw.githubusercontent.com/screwberry/screwberry-cloud/master/installAll.sh"],
+    "commandToExecute": "sh installAll.sh"
+  }
+SETTINGS
+
+  tags = {
+    project     = "screwberry"
+    environment = "${var.environment}"
+  }
 }
